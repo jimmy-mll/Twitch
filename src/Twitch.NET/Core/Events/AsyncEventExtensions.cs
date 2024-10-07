da12c0530b36bb6596e4f9fc9cbdd49c71e573c3
Aerafal <jimmy.mll@hotmail.com>
2024-10-07 14:35:54 +0200

    feat: add async event processor

---
+++ src/Twitch.NET/Core/Events/AsyncEventExtensions.cs
@@ -0,0 +1,28 @@
+﻿namespace Twitch.NET.Core.Events;
+
+/// <summary>
+///     Provides extension methods for asynchronous event handling.
+/// </summary>
+public static class AsyncEventExtensions
+{
+    /// <summary>
+    ///     Invokes an asynchronous event handler with the specified sender and event arguments.
+    /// </summary>
+    /// <typeparam name="TEventArgs">The type of the event arguments.</typeparam>
+    /// <param name="handler">The asynchronous event handler to invoke.</param>
+    /// <param name="sender">The source of the event.</param>
+    /// <param name="e">The event arguments.</param>
+    public static async ValueTask InvokeAsync<TEventArgs>(this AsyncEventHandler<TEventArgs>? handler, object sender, TEventArgs e)
+        where TEventArgs : AsyncEventArgs
+    {
+        if (handler is null)
+        {
+            return;
+        }
+
+        foreach (Delegate invocation in handler.GetInvocationList())
+        {
+            await ((AsyncEventHandler<TEventArgs>)invocation).InvokeAsync(sender, e).ConfigureAwait(false);
+        }
+    }
+}
