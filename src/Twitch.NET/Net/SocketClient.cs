4ab48f28ff6558783ec006838b50bc7deb0dc4d8
Aerafal <jimmy.mll@hotmail.com>
2024-10-07 17:57:00 +0200

    feat: remove the old network and use Andromeda.Framework

---
+++ src/Twitch.NET/Net/SocketClient.cs
@@ -0,0 +1,120 @@
+﻿using System.IO.Pipelines;
+using System.Net;
+using System.Text;
+using Bedrock.Framework;
+using Microsoft.AspNetCore.Connections;
+using Microsoft.Extensions.Options;
+
+namespace Twitch.NET.Net;
+
+/// <summary>
+/// Represents a client that connects to Twitch IRC chat.
+/// </summary>
+public sealed class SocketClient : IAsyncDisposable
+{
+    private const string Host = "irc.chat.twitch.tv";
+    private const int Port = 6667;
+    private const int SslPort = 6697;
+
+    private readonly TwitchClientOptions _options;
+    private ConnectionContext? _connection;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="SocketClient"/> class.
+    /// </summary>
+    /// <param name="options">The options for the socket client.</param>
+    public SocketClient(IOptions<TwitchClientOptions> options) => _options = options.Value;
+
+    /// <summary>
+    /// Disposes the connection asynchronously.
+    /// </summary>
+    public ValueTask DisposeAsync() => _connection?.DisposeAsync() ?? ValueTask.CompletedTask;
+
+    /// <summary>
+    /// Connects to the Twitch IRC server.
+    /// </summary>
+    /// <param name="cancellationToken">A token to cancel the operation.</param>
+    /// <returns>The task result contains a boolean indicating success or failure.</returns>
+    public async Task<bool> ConnectAsync(CancellationToken cancellationToken = default)
+    {
+        IPAddress[] addresses = await Dns.GetHostAddressesAsync(Host, cancellationToken).ConfigureAwait(false);
+        IPAddress address = addresses[0];
+        int port = _options.UseSsl ? SslPort : Port;
+
+        ClientBuilder? builder = new ClientBuilder(_options.Services)
+            .UseSockets()
+            .UseConnectionLogging();
+
+        if (_options.UseSsl)
+        {
+            builder.UseClientTls(tls => { tls.AllowAnyRemoteCertificate(); });
+        }
+
+        _connection = await builder
+            .Build()
+            .ConnectAsync(new IPEndPoint(address, port), cancellationToken)
+            .ConfigureAwait(false);
+
+        if (_connection is null)
+        {
+            return false;
+        }
+
+        await SendCommandAsync($"PASS {_options.TwitchOAuthToken}");
+        await SendCommandAsync($"NICK {_options.TwitchUsername}");
+        await SendCommandAsync($"USER {_options.TwitchUsername} 0 * :{_options.TwitchUsername.ToLower()}");
+
+        if (_options.Flags.HasFlag(TwitchIrcFlags.Membership))
+        {
+            await SendCommandAsync("CAP REQ twitch.tv/membership");
+        }
+
+        if (_options.Flags.HasFlag(TwitchIrcFlags.Tags))
+        {
+            await SendCommandAsync("CAP REQ twitch.tv/tags");
+        }
+
+        if (_options.Flags.HasFlag(TwitchIrcFlags.Commands))
+        {
+            await SendCommandAsync("CAP REQ twitch.tv/commands");
+        }
+
+        return true;
+    }
+
+    /// <summary>
+    /// Sends a command to the Twitch IRC server.
+    /// </summary>
+    /// <param name="command">The command to send.</param>
+    public ValueTask SendCommandAsync(string command)
+    {
+        if (_connection is null)
+        {
+            return ValueTask.CompletedTask;
+        }
+
+        if (_connection.ConnectionClosed.IsCancellationRequested)
+        {
+            return ValueTask.CompletedTask;
+        }
+
+        byte[] buffer = Encoding.UTF8.GetBytes($"{command}\r\n");
+        ValueTask<FlushResult> writeVt = _connection.Transport.Output.WriteAsync(buffer, _connection.ConnectionClosed);
+
+        return writeVt.IsCompletedSuccessfully
+            ? ValueTask.CompletedTask
+            : AwaitedSendCommandAsync(writeVt);
+
+        static async ValueTask AwaitedSendCommandAsync(ValueTask<FlushResult> writeVt)
+        {
+            try
+            {
+                await writeVt.ConfigureAwait(false);
+            }
+            catch
+            {
+                // ignored
+            }
+        }
+    }
+}
---
+++ src/Twitch.NET/TwitchClientOptions.cs
@@ -0,0 +1,43 @@
+﻿namespace Twitch.NET;
+
+/// <summary>
+/// Represents the options for configuring the Twitch client.
+/// </summary>
+public sealed class TwitchClientOptions
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="TwitchClientOptions"/> class.
+    /// </summary>
+    /// <param name="services">The service provider for dependency injection.</param>
+    internal TwitchClientOptions(IServiceProvider services) => Services = services;
+
+    /// <summary>
+    /// Gets the service provider for dependency injection.
+    /// </summary>
+    internal IServiceProvider Services { get; }
+
+    /// <summary>
+    /// Gets or sets the OAuth token for authenticating with Twitch.
+    /// </summary>
+    public required string TwitchOAuthToken { get; set; }
+
+    /// <summary>
+    /// Gets or sets the username for the Twitch account.
+    /// </summary>
+    public required string TwitchUsername { get; set; }
+
+    /// <summary>
+    /// Gets or sets the channel name to connect to on Twitch.
+    /// </summary>
+    public required string TwitchChannel { get; set; }
+
+    /// <summary>
+    /// Gets or sets a value indicating whether to use SSL for the connection.
+    /// </summary>
+    public required bool UseSsl { get; set; }
+
+    /// <summary>
+    /// Gets or sets the capabilities to request from the Twitch IRC server.
+    /// </summary>
+    public required TwitchIrcFlags Flags { get; set; }
+}
