c7ea519ebe929c6094adc70a0ecaf4fd0f7ddaac
Aerafal <jimmy.mll@hotmail.com>
2024-10-10 21:49:16 +0200

    feat: add twitch markdown

---
+++ src/Twitch/Alignment.cs
@@ -0,0 +1,22 @@
+﻿namespace Twitch;
+
+/// <summary>
+/// Represents the available alignments for Twitch.
+/// </summary>
+public enum Alignment
+{
+    /// <summary>
+    /// Left alignment.
+    /// </summary>
+    Left,
+
+    /// <summary>
+    /// Center alignment.
+    /// </summary>
+    Center,
+
+    /// <summary>
+    /// Right alignment.
+    /// </summary>
+    Right
+}
---
+++ src/Twitch/Border.cs
@@ -0,0 +1,47 @@
+﻿namespace Twitch;
+
+/// <summary>
+/// Represents the available border styles for Twitch.
+/// </summary>
+public enum Border
+{
+    /// <summary>
+    /// Solid border style.
+    /// </summary>
+    Solid,
+
+    /// <summary>
+    /// Dashed border style.
+    /// </summary>
+    Dashed,
+
+    /// <summary>
+    /// Dotted border style.
+    /// </summary>
+    Dotted,
+
+    /// <summary>
+    /// Double border style.
+    /// </summary>
+    Double,
+
+    /// <summary>
+    /// Groove border style.
+    /// </summary>
+    Groove,
+
+    /// <summary>
+    /// Ridge border style.
+    /// </summary>
+    Ridge,
+
+    /// <summary>
+    /// Inset border style.
+    /// </summary>
+    Inset,
+
+    /// <summary>
+    /// Outset border style.
+    /// </summary>
+    Outset
+}
