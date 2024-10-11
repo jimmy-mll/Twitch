1ce283c290729b0653feb08d52738e848a7368ec
Aerafal <jimmy.mll@hotmail.com>
2024-10-11 09:17:13 +0200

    doc: add real documentation for TwitchOAuthClientOptions

--- src/Twitch/OAuth/TwitchOAuthClientOptions.cs
+++ src/Twitch/OAuth/TwitchOAuthClientOptions.cs
@@ -3,37 +3,41 @@
 namespace Twitch.OAuth;
 
 /// <summary>
-/// Represents the options for configuring the Twitch OAuth client.
+/// Specify the following query parameters as appropriate for your application.
 /// </summary>
 public sealed class TwitchOAuthClientOptions
 {
     /// <summary>
-    /// Gets or sets the client ID.
+    /// Your app’s <a href="https://dev.twitch.tv/docs/authentication/register-app">registered</a> client ID.
     /// </summary>
     public required string ClientId { get; set; }
 
     /// <summary>
-    /// Gets or sets the client secret.
+    /// Your app’s <a href="https://dev.twitch.tv/docs/authentication/register-app">registered</a> client secret.
     /// </summary>
     public required string ClientSecret { get; set; }
 
     /// <summary>
-    /// Gets or sets the redirect URI.
+    /// Your app’s registered redirect URI. The authorization code is sent to this URI.
     /// </summary>
-    public required string RedirectUri { get; set; }
+    public required Uri RedirectUri { get; set; }
 
     /// <summary>
-    /// Gets or sets the OAuth scopes.
+    /// A space-delimited list of scopes. The APIs that you’re calling will identify the scopes you must list. You must URL encode the list.
     /// </summary>
     public required OAuthScope[] Scopes { get; set; }
 
     /// <summary>
-    /// Gets or sets a value indicating whether to force verification.
+    /// Set to <b>true</b> to force the user to re-authorize your app’s access to their resources. The default is <b>false</b>.
     /// </summary>
     public required bool ForceVerify { get; set; }
 
     /// <summary>
-    /// Gets or sets the state.
+    /// Although optional, you are strongly encouraged to pass a state string to help prevent
+    /// <a href="https://datatracker.ietf.org/doc/html/rfc6749#section-10.12">Cross-Site Request Forgery</a> (CSRF) attacks.
+    /// The server returns this string to you in your redirect URI (see the state parameter in the fragment portion of the URI).
+    /// If this string doesn’t match the state string that you passed, ignore the response.
+    /// The state string should be randomly generated and unique for each OAuth request.
     /// </summary>
     public required string State { get; set; }
 }
